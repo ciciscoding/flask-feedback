3bf73ac2fd50b3b83c3672b6f8b4792396fce7a3
Cici Thomas <cici@thomas.com>
2020-12-22 02:02:52 -0500

    fix secret key

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flask_feedback_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', SECRET_KEY)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'thisismysecretkey321')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
